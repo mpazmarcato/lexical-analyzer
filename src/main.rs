3c719552a424b59a0f29d68b83a6aa8668da9897
Maria Paz <mpazmarcato@gmail.com>
2025-10-17 22:41:07 -0300

    feat: add more example expressions to lexer

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,37 @@ use iteradores::*;
 use analisador::*;
 
 fn main() {
-    let mut entrada = "450 + 20";
-    let mut offset = 0;
+    let exemplos = [
+        "450 + 20",
+        "450     +     20",
+        "450+20",
+        "0+-0",
+        "0 +++",
+        "10+a",
+        "10 + 20a",
+    ];
 
-    loop {
-        match proximo(entrada, offset) {
-            Ok((pos, token, resto, novo_offset)) => {
-                println!("Token: {}, posição: {}", token, pos);
-                entrada = resto;
-                offset = novo_offset;
-            }
-            Err(None) => {
-                println!("Fim da análise");
-                break;
-            }
-            Err(Some(pos)) => {
-                println!("Erro na posição {}", pos);
-                break;
+    for mut entrada in exemplos {
+        println!("{}", entrada);
+        let mut offset = 0;
+
+        loop {
+            match proximo(entrada, offset) {
+                Ok((pos, token, resto, novo_offset)) => {
+                    println!("Token: {}, posição: {}", token, pos);
+                    entrada = resto;
+                    offset = novo_offset;
+                }
+                Err(None) => {
+                    println!("Fim da análise");
+                    break;
+                }
+                Err(Some(pos)) => {
+                    println!("Erro na posição {}", pos);
+                    break;
+                }
             }
         }
+        println!();
     }
 }
